09aea36859bb0d806eb4085a2c154cb781bd8345
Chris Beams <cbeams@vmware.com>
2011-06-30 16:40:42 -0700

    Add repro case (and workaround) for SPR-6428
    Successfully reproduces reported error:
    
        java.io.FileNotFoundException: class path resource
        [${resourceDirPlaceHolder}/props.properties] cannot be opened
        because it does not exist
    
    And provides a workaround @Test that demonstrates using a
    PropertySource to avoid the lifecycle issue that causes the exception
    above.
    
    Issue: SPR-6428

---
+++ SPR-6428/src/test/java/ReproTests.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2002-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.core.env.MapPropertySource;
+
+/**
+ * Unit tests demonstrating the failure reported by SPR-6428, and the workaround using
+ * Spring's Environment and PropertySource APIs that fixes it.
+ *
+ * @author Chris Beams
+ */
+public class ReproTests {
+
+	/**
+	 * Test will fail as two PropertyPlaceholderConfigurer beans are defined, with one
+	 * depending on the other for placeholder resolution.  This is a lifecycle issue with
+	 * the way BeanFactoryPostProcessors are processed.  See {@link #workaround()} below
+	 * for a solution to this.
+	 */
+	@Test
+	public void repro() {
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		ctx.load("ReproTests-repro.xml");
+		ctx.refresh();
+	}
+
+	/**
+	 * Work around the lifecycle issues above by introducing a PropertySource containing
+	 * the value for the "resourceDirPlaceHolder"
+	 */
+	@Test
+	public void workaround() {
+		Map<String,Object> localProps = new HashMap<String,Object>();
+		localProps.put("resourceDirPlaceHolder", "myResourceDir");
+
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		ctx.getEnvironment()
+			.getPropertySources()
+				.addFirst(new MapPropertySource("localProps", localProps));
+		ctx.load("ReproTests-workaround.xml");
+		ctx.refresh();
+	}
+
+}
