3832bb236443ee346a4afc8421c3b92e799dabec
Chris Beams <cbeams@vmware.com>
2011-09-04 17:06:18 -0700

    Add convenience script for creating repro projects
        ./create-repro-project.sh SPR-9876
    
    will create a new project based on the default SPR-0000 template
    
        ./create-repro-project.sh SPR-0000-war-xml SPR-9878
    
    will create a new project based on the SPR-0000-war-xml template
    
    In any case, only the pom.xml file and contents of 'src' directory are
    copied into the new project, avoiding any inadvertent copying of
    'target' directory, eclipse metadata or other transient files.
    
    Finally, the newly copied pom.xml is processed to replace the
    original artifactId with that of the new project directory name.
    
    After this script is run, the new project directory should be directly
    importable into Eclipse/STS/IDEA as an existing Maven project.

---
+++ create-repro-project.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+    echo "usage: $0 [sourcedir] jira-id"
+    exit 1
+fi
+if [ -z "$2" ]; then
+    source_dir='SPR-0000' # default
+    target_dir=$1
+else
+    source_dir=$1
+    target_dir=$2
+fi
+if [ ! -d "$source_dir" ]; then
+    echo "source directory $source_dir does not exist"
+    exit 2
+fi
+if [ -d "$target_dir" ]; then
+    echo "target directory $target_dir already exists"
+    exit 3
+fi
+expr="s/$source_dir/$target_dir/"
+set -x
+mkdir $target_dir
+cp -r $source_dir/src $target_dir/
+cp $source_dir/pom.xml $target_dir/
+perl -p -i -e $expr $target_dir/pom.xml
