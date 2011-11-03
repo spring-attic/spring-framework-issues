68c8e06ffb13f3cc7ac83be2bdc2c4076ba132be
Chris Beams <cbeams@vmware.com>
2011-11-03 16:20:01 -0700

    Simplify further
    Everything based on @Configuration classes, no XML. Register everything
    explicitly; no component scanning. Remove MyPropertyPlaceholderConfigurer,
    my.properties etc; @Value injection still occurs without them. Remove
    MyMethodInterceptor; AutoProxyCreation still occurs for foo and bar
    beans without it.

--- SPR-8813/src/test/java/org/springframework/issues/ReproTests.java
+++ SPR-8813/src/test/java/org/springframework/issues/ReproTests.java
@@ -1,16 +1,14 @@
 package org.springframework.issues;
 
-import java.util.Properties;
+import static org.springframework.beans.factory.config.AutowireCapableBeanFactory.AUTOWIRE_BY_NAME;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.aop.framework.autoproxy.BeanNameAutoProxyCreator;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
-import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.aop.support.AopUtils;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.ClassUtils;
 
 /**
  * Unit test that reproduces an issue reported against SPR JIRA. @Test methods within
@@ -19,99 +17,52 @@ import org.springframework.util.ClassUtils;
  */
 public class ReproTests {
 
-	@Configuration
-	static class PPCConfig {
-		@Bean
-		public static PropertyPlaceholderConfigurer ppc() {
-			PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
-			Properties props = new Properties();
-			props.setProperty("issues.test", "whatev");
-			ppc.setProperties(props);
-			return ppc;
-		}
-	}
-
 	@Test
 	public void testPlainObjectSucess() {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-		ctx.register(Config1.class);
+		ctx.register(Baz.class);
 		ctx.refresh();
-		
-		Baz bean = ctx.getBean(Baz.class);
 
+		Baz bean = ctx.getBean(Baz.class);
 		Assert.assertEquals(bean.getClass(), Baz.class);
 		Assert.assertEquals(1, bean.getValueCounter());
-
-		ctx.getBeanFactory().autowireBeanProperties(bean, AutowireCapableBeanFactory.AUTOWIRE_BY_NAME, false);
-
+		ctx.getBeanFactory().autowireBeanProperties(bean, AUTOWIRE_BY_NAME, false);
 		Assert.assertEquals(2, bean.getValueCounter());
-	}
-
-	@Configuration
-	static class Config1 extends PPCConfig {
-		@Bean
-		public Baz baz() {
-			return new Baz();
-		}
 	}
 
 	@Test
 	public void testJdkProxyObjectFail() {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-		ctx.register(Config2.class);
+		ctx.register(BarImpl.class, APC_Config.class);
 		ctx.refresh();
 
 		Bar bean = ctx.getBean(Bar.class);
-
+		Assert.assertTrue(AopUtils.isJdkDynamicProxy(bean));
 		Assert.assertEquals(1, bean.getValueCounter());
-		Assert.assertFalse( ClassUtils.isCglibProxy( bean ) );
-
-		ctx.getBeanFactory().autowireBeanProperties(bean, AutowireCapableBeanFactory.AUTOWIRE_BY_NAME, false);
-
+		ctx.getBeanFactory().autowireBeanProperties(bean, AUTOWIRE_BY_NAME, false);
 		Assert.assertEquals(2, bean.getValueCounter());
-
-	}
-	@Configuration
-	static class Config2 extends PPCConfig {
-		@Bean
-		public Bar bar() {
-			return new BarImpl();
-		}
-		@Bean
-		public BeanNameAutoProxyCreator apc() {
-			BeanNameAutoProxyCreator apc = new BeanNameAutoProxyCreator();
-			apc.setBeanNames(new String[] { "bar" });
-			return apc;
-		}
 	}
 
 	@Test
 	public void testCglibProxyObjectFail() {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-		ctx.register(Config3.class);
+		ctx.register(Foo.class, APC_Config.class);
 		ctx.refresh();
 
 		Foo bean = ctx.getBean(Foo.class);
-
+		Assert.assertTrue(AopUtils.isCglibProxy( bean ));
 		Assert.assertEquals(1, bean.getValueCounter());
-		Assert.assertTrue( ClassUtils.isCglibProxy( bean ) );
-
-		ctx.getBeanFactory().autowireBeanProperties(bean, AutowireCapableBeanFactory.AUTOWIRE_BY_NAME, false);
-
+		ctx.getBeanFactory().autowireBeanProperties(bean, AUTOWIRE_BY_NAME, false);
 		Assert.assertEquals(2, bean.getValueCounter());
 	}
+
 	@Configuration
-	static class Config3 extends PPCConfig {
-		@Bean
-		public Foo foo() {
-			return new Foo();
-		}
+	static class APC_Config {
 		@Bean
 		public BeanNameAutoProxyCreator apc() {
 			BeanNameAutoProxyCreator apc = new BeanNameAutoProxyCreator();
-			apc.setBeanNames(new String[] { "foo" });
+			apc.setBeanNames(new String[] { "foo", "bar" });
 			return apc;
 		}
 	}
-
 }
