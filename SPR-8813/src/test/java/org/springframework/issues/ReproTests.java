9af336d416dda1d5cf7657d67802333a13d190c1
Chris Beams <cbeams@vmware.com>
2011-11-03 13:24:29 -0700

    Simplify

--- SPR-8813/src/test/java/org/springframework/issues/ReproTests.java
+++ SPR-8813/src/test/java/org/springframework/issues/ReproTests.java
@@ -1,8 +1,15 @@
 package org.springframework.issues;
 
+import java.util.Properties;
+
 import org.junit.Assert;
 import org.junit.Test;
-import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.aop.framework.autoproxy.BeanNameAutoProxyCreator;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ClassUtils;
 
 /**
@@ -12,59 +19,99 @@ import org.springframework.util.ClassUtils;
  */
 public class ReproTests {
 
-    @Test
-    public void testPlainObjectSucess() {
-        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
-        ctx.load("classpath:org/springframework/issues/ReproTests-context.xml");
-        ctx.refresh();
+	@Configuration
+	static class PPCConfig {
+		@Bean
+		public static PropertyPlaceholderConfigurer ppc() {
+			PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
+			Properties props = new Properties();
+			props.setProperty("issues.test", "whatev");
+			ppc.setProperties(props);
+			return ppc;
+		}
+	}
 
-        Baz bean = ctx.getBean(Baz.class);
+	@Test
+	public void testPlainObjectSucess() {
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+		ctx.register(Config1.class);
+		ctx.refresh();
+		
+		Baz bean = ctx.getBean(Baz.class);
 
-        Assert.assertTrue( bean.getClass() == Baz.class );
-        Assert.assertEquals(1, bean.getValueCounter());
+		Assert.assertEquals(bean.getClass(), Baz.class);
+		Assert.assertEquals(1, bean.getValueCounter());
 
-        MyPropertyPlaceholderConfigurer ppc = ctx.getBean( MyPropertyPlaceholderConfigurer.class );
-        ppc.reload();
+		ctx.getBeanFactory().autowireBeanProperties(bean, AutowireCapableBeanFactory.AUTOWIRE_BY_NAME, false);
 
-        Assert.assertEquals(2, bean.getValueCounter());
+		Assert.assertEquals(2, bean.getValueCounter());
+	}
 
-    }
+	@Configuration
+	static class Config1 extends PPCConfig {
+		@Bean
+		public Baz baz() {
+			return new Baz();
+		}
+	}
 
 	@Test
 	public void testJdkProxyObjectFail() {
-		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
-		ctx.load("classpath:org/springframework/issues/ReproTests-context.xml");
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+		ctx.register(Config2.class);
 		ctx.refresh();
 
 		Bar bean = ctx.getBean(Bar.class);
 
-		Assert.assertFalse( ClassUtils.isCglibProxy( bean ) );
-		Assert.assertFalse( bean.getClass() == BarImpl.class );
 		Assert.assertEquals(1, bean.getValueCounter());
+		Assert.assertFalse( ClassUtils.isCglibProxy( bean ) );
 
-		MyPropertyPlaceholderConfigurer ppc = ctx.getBean( MyPropertyPlaceholderConfigurer.class );
-		ppc.reload();
+		ctx.getBeanFactory().autowireBeanProperties(bean, AutowireCapableBeanFactory.AUTOWIRE_BY_NAME, false);
 
 		Assert.assertEquals(2, bean.getValueCounter());
 
 	}
+	@Configuration
+	static class Config2 extends PPCConfig {
+		@Bean
+		public Bar bar() {
+			return new BarImpl();
+		}
+		@Bean
+		public BeanNameAutoProxyCreator apc() {
+			BeanNameAutoProxyCreator apc = new BeanNameAutoProxyCreator();
+			apc.setBeanNames(new String[] { "bar" });
+			return apc;
+		}
+	}
 
 	@Test
-    public void testCglibProxyObjectFail() {
-        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
-        ctx.load("classpath:org/springframework/issues/ReproTests-context.xml");
-        ctx.refresh();
+	public void testCglibProxyObjectFail() {
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+		ctx.register(Config3.class);
+		ctx.refresh();
 
-        Foo bean = ctx.getBean(Foo.class);
+		Foo bean = ctx.getBean(Foo.class);
 
-        Assert.assertTrue( ClassUtils.isCglibProxy( bean ) );
-        Assert.assertEquals(1, bean.getValueCounter());
+		Assert.assertEquals(1, bean.getValueCounter());
+		Assert.assertTrue( ClassUtils.isCglibProxy( bean ) );
 
-        MyPropertyPlaceholderConfigurer ppc = ctx.getBean( MyPropertyPlaceholderConfigurer.class );
-        ppc.reload();
+		ctx.getBeanFactory().autowireBeanProperties(bean, AutowireCapableBeanFactory.AUTOWIRE_BY_NAME, false);
 
-        Assert.assertEquals(2, bean.getValueCounter());
-
-    }
+		Assert.assertEquals(2, bean.getValueCounter());
+	}
+	@Configuration
+	static class Config3 extends PPCConfig {
+		@Bean
+		public Foo foo() {
+			return new Foo();
+		}
+		@Bean
+		public BeanNameAutoProxyCreator apc() {
+			BeanNameAutoProxyCreator apc = new BeanNameAutoProxyCreator();
+			apc.setBeanNames(new String[] { "foo" });
+			return apc;
+		}
+	}
 
 }
