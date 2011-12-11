ea9fd1f6217a609557661706429c8e58a0c89af8
Chris Beams <cbeams@vmware.com>
2011-12-11 11:23:17 +0100

    Prove SPR-8761 is fixed against 3.1.0 snapshot

--- SPR-8761/src/main/java/com/example/UsesAnnotation.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-/** Should be component-scanned. */
-@WithNestedAnnotation.MyComponent
-public class UsesAnnotation {
-
-}
--- SPR-8761/src/test/java/com/example/ComponentScanTest.java
+++ SPR-8761/src/test/java/com/example/ComponentScanTest.java
@@ -1,19 +1,47 @@
 package com.example;
 
-import static org.junit.Assert.assertNotNull;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
+import org.springframework.stereotype.Component;
 
 public class ComponentScanTest {
 
-	/** Tests that the dependency is injected into the abstract base class. */
 	@Test
-	public void testDependencyInjected() {
-		ApplicationContext ctx = new GenericXmlApplicationContext("/applicationContext.xml");
-		UsesAnnotation underTest = ctx.getBean(UsesAnnotation.class);
-		assertNotNull(underTest);
+	public void withScanner() {
+		DefaultListableBeanFactory bf = new DefaultListableBeanFactory();
+		ClassPathBeanDefinitionScanner scanner = new ClassPathBeanDefinitionScanner(bf);
+		scanner.scan("com.example");
+		for (String name : bf.getBeanDefinitionNames()) {
+			//System.out.println(name);
+		}
 	}
 
+	@Test
+	public void withACAC() {
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+		ctx.scan("com.example");
+		ctx.refresh();
+	}
+
+}
+
+@Component
+class WithNestedAnnotation {
+
+	@Target({ ElementType.TYPE })
+	@Retention(RetentionPolicy.RUNTIME)
+	@Documented
+	@Component
+	public static @interface MyComponent {
+
+		String value() default "";
+	} 
 }
