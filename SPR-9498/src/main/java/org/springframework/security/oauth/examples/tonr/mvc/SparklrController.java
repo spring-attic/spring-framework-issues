68a832330ee53fa34a2b65fdee090f56ba089f52
Dave Syer <dsyer@vmware.com>
2012-06-14 07:23:24 +0100

    Strip down sample so it breaks on /photos?foo=bar

--- SPR-9498/src/main/java/org/springframework/security/oauth/examples/tonr/mvc/SparklrController.java
+++ SPR-9498/src/main/java/org/springframework/security/oauth/examples/tonr/mvc/SparklrController.java
@@ -1,72 +1,29 @@
 package org.springframework.security.oauth.examples.tonr.mvc;
 
-import java.awt.image.BufferedImage;
-import java.io.InputStream;
-import java.util.Iterator;
+import java.net.URI;
 
-import javax.imageio.ImageIO;
-import javax.imageio.ImageReadParam;
-import javax.imageio.ImageReader;
-import javax.imageio.stream.MemoryCacheImageInputStream;
-import javax.servlet.UnavailableException;
-
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.security.oauth.examples.tonr.SparklrService;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestOperations;
 
 /**
- * @author Ryan Heaton
  * @author Dave Syer
  */
 @Controller
 public class SparklrController {
 
-	private SparklrService sparklrService;
+	private RestOperations restTemplate;
 
-	@RequestMapping("/sparklr/photos")
+	public void setRestTemplate(OAuth2RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
+	@RequestMapping("/photos")
 	public String photos(Model model) throws Exception {
-		model.addAttribute("photoIds", sparklrService.getSparklrPhotoIds());
+		restTemplate.getForObject(URI.create("http://some/photos"), byte[].class);
 		return "sparklr";
-	}
-
-	@RequestMapping("/sparklr/photos/{id}")
-	public ResponseEntity<BufferedImage> photo(@PathVariable String id) throws Exception {
-		InputStream photo = sparklrService.loadSparklrPhoto(id);
-		if (photo == null) {
-			throw new UnavailableException("The requested photo does not exist");
-		}
-		BufferedImage body;
-		MediaType contentType = MediaType.IMAGE_JPEG;
-		Iterator<ImageReader> imageReaders = ImageIO.getImageReadersByMIMEType(contentType.toString());
-		if (imageReaders.hasNext()) {
-			ImageReader imageReader = imageReaders.next();
-			ImageReadParam irp = imageReader.getDefaultReadParam();
-			imageReader.setInput(new MemoryCacheImageInputStream(photo), true);
-			body = imageReader.read(0, irp);
-		} else {
-			throw new HttpMessageNotReadableException("Could not find javax.imageio.ImageReader for Content-Type ["
-					+ contentType + "]");
-		}
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.IMAGE_JPEG);
-		return new ResponseEntity<BufferedImage>(body, headers, HttpStatus.OK);
-	}
-
-	@RequestMapping("/trusted/message")
-	public String trusted(Model model) throws Exception {
-		model.addAttribute("message", this.sparklrService.getTrustedMessage());
-		return "home";
-	}
-
-	public void setSparklrService(SparklrService sparklrService) {
-		this.sparklrService = sparklrService;
 	}
 
 }
