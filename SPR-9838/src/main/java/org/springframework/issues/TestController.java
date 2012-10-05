cbfd846b3f7742685d3461e101d7cdabce4125db
Rossen Stoyanchev <rstoyanchev@vmware.com>
2012-10-05 10:28:41 -0400

    Update SPR-9838

--- SPR-9838/src/main/java/org/springframework/issues/TestController.java
+++ SPR-9838/src/main/java/org/springframework/issues/TestController.java
@@ -15,21 +15,19 @@
  */
 package org.springframework.issues;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.HandlerMapping;
 
 @Controller
 public class TestController {
 
-	@RequestMapping(value = "/fixidentity/"+"**", method = RequestMethod.GET)
+	@RequestMapping(value = "/fixidentity/{tagValues}", method = RequestMethod.GET)
 	@ResponseBody
-	public void getIdentityByTagValue(HttpServletRequest request) {
-	    System.out.println(request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE));
+	public void getIdentityByTagValue(@PathVariable String tagValues) {
+	    System.out.println(tagValues);
 	}
 
 }
