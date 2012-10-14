48cb6f314e27fa18aa3b5b64c02007df7d9c64cf
Rossen Stoyanchev <rstoyanchev@vmware.com>
2012-10-14 11:44:36 -0400

    Update SPR-9832

---
+++ SPR-9832/src/test/java/org/springframework/issues/RestTemplateTests.java
@@ -0,0 +1,37 @@
+package org.springframework.issues;
+
+import static org.junit.Assert.assertEquals;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.client.CommonsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+public class RestTemplateTests {
+
+    private RestTemplate restTemplate;
+
+    @Before
+    public void setUp() {
+        HttpClient httpClient = new HttpClient();
+		this.restTemplate = new RestTemplate(new CommonsClientHttpRequestFactory(httpClient));
+    }
+
+    @Test
+    public void google() {
+        String url = "http://www.google.com/ig/calculator?q=1USD=?EUR";
+		String response = this.restTemplate.getForObject(url, String.class);
+
+        assertEquals("{lhs: \"1 U.S. dollar\",rhs: \"1 U.S. dollar\",error: \"\",icc: true}", response);
+    }
+
+    @Test
+    public void localhost() {
+        String url = "http://localhost:8080/SPR-9832/calculator";
+		String response = this.restTemplate.getForObject(url, String.class);
+
+		assertEquals("{lhs: \"1 U.S. dollar\",rhs: \"1 U.S. dollar\",error: \"\",icc: true}", response);
+    }
+
+}
