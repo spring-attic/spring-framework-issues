429a69e6ccf37b0966c16469fdbb0a24eb8416c5
Chris Beams <cbeams@vmware.com>
2013-01-21 15:42:20 +0100

    Simply and prove still fails under JDK 6
    JDK 7 always passes

--- introspectionproblem/src/test/java/com/demo/IntrospectionJavaTests.java
+++ introspectionproblem/src/test/java/com/demo/IntrospectionJavaTests.java
@@ -1,20 +1,20 @@
 package com.demo;
 
 import org.codehaus.groovy.grails.commons.spring.GrailsWebApplicationContext;
-import org.codehaus.groovy.grails.web.servlet.mvc.GrailsWebRequest;
 import org.junit.Test;
 import org.springframework.beans.BeanWrapperImpl;
 
 public class IntrospectionJavaTests {
+
 	@Test
 	public void testSomething() {
 		Object ctx = new GrailsWebApplicationContext();
 		BeanWrapperImpl wrapper = new BeanWrapperImpl(ctx);
 		assert wrapper.isReadableProperty("autowireCapableBeanFactory");
 	}
-	
-	public static GrailsWebRequest getMessageSource() {
-        return null;
+
+	void getMessageSource() {
+		//return null;
 	}
 }
 
