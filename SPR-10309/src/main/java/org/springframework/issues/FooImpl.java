7421bd12e678c627dccffc2648b3c6159fc449db
Dejan Predovic <dpredovic@gmail.com>
2013-02-16 22:19:55 +0100

    SPR-10309

---
+++ SPR-10309/src/main/java/org/springframework/issues/FooImpl.java
@@ -0,0 +1,38 @@
+package org.springframework.issues;
+
+import org.hibernate.validator.internal.engine.MethodConstraintViolationImpl;
+import org.hibernate.validator.method.MethodConstraintViolation;
+import org.hibernate.validator.method.MethodConstraintViolationException;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.Collections;
+
+@Component
+@Validated
+public class FooImpl implements Foo {
+
+    @Override
+    public void bar(String s) throws ValidationException {
+        System.out.println("s = " + s);
+    }
+
+    @Override
+    public void bar() throws ValidationException {
+        throw new MethodConstraintViolationException(Collections
+                                                         .singleton(new MethodConstraintViolationImpl<MethodConstraintViolation<?>>(
+                                                             null,
+                                                             null,
+                                                             null,
+                                                             null,
+                                                             null,
+                                                             null,
+                                                             null,
+                                                             null,
+                                                             null,
+                                                             null,
+                                                             null,
+                                                             null)));
+    }
+
+}
