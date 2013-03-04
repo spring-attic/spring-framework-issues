2a4707044b1cb3f264c89bcf02553599b7888aa2
Chris Beams <cbeams@vmware.com>
2013-03-04 13:09:38 +0100

    Rename resources for clarity; demonstrate override

--- SPR-10344/src/test/java/org/springframework/issues/ReproTests.java
+++ SPR-10344/src/test/java/org/springframework/issues/ReproTests.java
@@ -25,45 +25,46 @@ public class ReproTests {
 
 	@BeforeClass
 	public static void setUpBeforeClass() {
-		System.setProperty("foo", "local");
+		System.setProperty("k1", "v1-system");
 	}
 
 	@Test
-	public void localPropertyPlaceholderOverrideFalse() {
-		localPropertiesOverride(false, "classpath:org/springframework/issues/PropertyPlaceholderConfigurerFalse-context.xml");
+	public void propertyPlaceholderWithLocalOverrideFalse() {
+		assertThat(v1("classpath:org/springframework/issues/PropertyPlaceholderConfigurerFalse-context.xml"), equalTo("v1-local"));
 	}
 
 	@Test
-	public void localPropertySourcesPlaceholderOverrideFalse() {
-		localPropertiesOverride(false, "classpath:org/springframework/issues/PropertySourcesPlaceholderConfigurerFalse-context.xml");
+	public void propertyPlaceholderWithLocalOverrideFalseAndSystemPropertiesModeOverride() {
+		assertThat(v1("classpath:org/springframework/issues/PropertyPlaceholderConfigurerFalse-SPMOverride-context.xml"), equalTo("v1-system"));
 	}
 
 	@Test
-	public void localPropertyPlaceholderOverrideTrue() {
-		localPropertiesOverride(true, "classpath:org/springframework/issues/PropertyPlaceholderConfigurerTrue-context.xml");
+	public void propertySourcesPlaceholderWithLocalOverrideFalse() {
+		assertThat(v1("classpath:org/springframework/issues/PropertySourcesPlaceholderConfigurerFalse-context.xml"), equalTo("v1-system"));
 	}
 
 	@Test
-	public void localPropertySourcesPlaceholderOverrideTrue() {
-		localPropertiesOverride(true, "classpath:org/springframework/issues/PropertySourcesPlaceholderConfigurerTrue-context.xml");
+	public void propertyPlaceholderWithLocalOverrideTrue() {
+		assertThat(v1("classpath:org/springframework/issues/PropertyPlaceholderConfigurerTrue-context.xml"), equalTo("v1-local"));
 	}
 
-	public void localPropertiesOverride(boolean override, String resourceLocation) {
+	@Test
+	public void propertySourcesPlaceholderWithLocalOverrideTrue() {
+		assertThat(v1("classpath:org/springframework/issues/PropertySourcesPlaceholderConfigurerTrue-context.xml"), equalTo("v1-local"));
+	}
+
+	public String v1(String resourceLocation) {
 
 		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
 		ctx.load(resourceLocation);
 		ctx.refresh();
 
-		assertThat(System.getProperty("foo"), equalTo("local"));
-
-		if (override) {
-			assertThat(ctx.getBean(TestBean.class).getName(), equalTo("local"));
+		try {
+			return ctx.getBean(TestBean.class).getName();
 		}
-		else {
-			assertThat(ctx.getBean(TestBean.class).getName(), equalTo("enclosing"));
+		finally {
+			ctx.close();
 		}
-
-		ctx.close();
 	}
 
 }
