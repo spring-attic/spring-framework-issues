8f49bb3dbf2eaa0c7bf1d68c2d8544481ef398c1
Phillip Webb <pwebb@vmware.com>
2013-06-26 18:00:49 -0700

    SPR-10549

---
+++ SPR-10549/src/main/java/org/springframework/issues/Foo.java
@@ -0,0 +1,25 @@
+
+package org.springframework.issues;
+
+public class Foo {
+
+	private String abc;
+
+	private String abc2;
+
+	public void setAbc(String abc) {
+		this.abc = abc;
+	}
+
+	public String getAbc() {
+		return abc;
+	}
+
+	public void setAbc2(String abc2) {
+		this.abc2 = abc2;
+	}
+
+	public String getAbc2() {
+		return abc2;
+	}
+}
---
+++ SPR-10549/src/test/java/org/springframework/issues/ReproTests.java
@@ -0,0 +1,37 @@
+
+package org.springframework.issues;
+
+import org.junit.Test;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test that reproduces an issue reported against SPR JIRA. @Test methods within need
+ * not pass with the green bar! Rather they should fail in such a way that demonstrates
+ * the reported issue.
+ */
+public class ReproTests {
+
+	@Test
+	public void works() throws Exception {
+		repro("ReproTests-context.xml");
+	}
+
+	@Test
+	public void fails() throws Exception {
+		repro("ReproTests-context-fails.xml");
+	}
+
+	private void repro(String xml) {
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		ctx.load("classpath:org/springframework/issues/" + xml);
+		ctx.refresh();
+
+		Foo foo = ctx.getBean(Foo.class);
+		assertEquals(foo.getAbc(), "${none}");
+		assertEquals(foo.getAbc2(), "test");
+		ctx.close();
+	}
+
+}
