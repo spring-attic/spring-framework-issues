af3c03ad60785034e47b8bd9124ffb69d25bf162
Gonçalo Silva and Nicholas Street <pair+gsilva+nstreet@pivotallabs.com>
2013-06-28 10:07:34 +0100

    SPR-10704 - mockMvx xpath does not work as expected
    - Test runs against a simple atom feed constructed using ROME

---
+++ SPR-10704/src/main/java/org/springframework/issues/BlogPostAtomViewer.java
@@ -0,0 +1,24 @@
+package org.springframework.issues;
+
+import com.sun.syndication.feed.atom.*;
+import org.springframework.web.servlet.view.feed.AbstractAtomFeedView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
+
+public class BlogPostAtomViewer extends AbstractAtomFeedView {
+
+	@Override
+	protected void buildFeedMetadata(Map<String, Object> model, Feed feed, HttpServletRequest request) {
+		feed.setTitle("Test Feed");
+		feed.setIcon("http://www.example.com/favicon.ico");
+		super.buildFeedMetadata(model, feed, request);
+	}
+
+
+	@Override
+	protected List<Entry> buildFeedEntries(Map<String, Object> stringObjectMap, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+		return null;
+	}
+}
