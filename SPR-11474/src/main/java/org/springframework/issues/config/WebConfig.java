1e4ef2beef684dcc330f5dec933f07392768032c
Rossen Stoyanchev <rstoyanchev@gopivotal.com>
2014-02-24 15:19:31 -0500

    Modify SPR-11474 project to actually reproduce issue

--- SPR-11474/src/main/java/org/springframework/issues/config/WebConfig.java
+++ SPR-11474/src/main/java/org/springframework/issues/config/WebConfig.java
@@ -5,13 +5,22 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-@EnableWebMvc
 @ComponentScan(basePackages="org.springframework.issues")
 @Configuration
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig extends WebMvcConfigurationSupport {
+
+	@Override
+	@Bean
+	public RequestMappingHandlerMapping requestMappingHandlerMapping() {
+		RequestMappingHandlerMapping mapping = super.requestMappingHandlerMapping();
+		mapping.setAlwaysUseFullPath(true);
+		return mapping;
+	}
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
