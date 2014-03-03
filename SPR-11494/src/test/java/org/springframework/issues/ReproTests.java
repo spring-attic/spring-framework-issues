2b546a7e1166f0a4f0a6e165b20c2258f3f5c1c2
Stephane Nicoll <snicoll@gopivotal.com>
2014-03-03 11:33:58 +0100

    Complete test for SPR-11494

--- SPR-11494/src/test/java/org/springframework/issues/ReproTests.java
+++ SPR-11494/src/test/java/org/springframework/issues/ReproTests.java
@@ -1,5 +1,10 @@
 package org.springframework.issues;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.util.List;
+
 import org.junit.Test;
 import org.springframework.expression.Expression;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
@@ -9,7 +14,9 @@ import org.springframework.expression.spel.standard.SpelExpressionParser;
 public class ReproTests {
 
 	@Test
-	public void spr11494() { // Expected to fail
-		new SpelExpressionParser().parseExpression("T(java.util.Arrays).asList('a','b')");
+	public void spr11494() {
+		Expression exp = new SpelExpressionParser().parseExpression("T(java.util.Arrays).asList('a','b')");
+		List<String> list = (List<String>) exp.getValue();
+		assertThat(list.size(), is(2));
 	}
 }
