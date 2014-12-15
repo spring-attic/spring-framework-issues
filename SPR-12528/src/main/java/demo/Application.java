d6b699cc7e07a4a1ede01ddeaccd8f8b3887697f
Sebastien Deleuze <sdeleuze@pivotal.io>
2014-12-15 11:23:31 +0100

    Add test project for SPR-12528

---
+++ SPR-12528/src/main/java/demo/Application.java
@@ -0,0 +1,34 @@
+package demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaBaseConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.EclipseLinkJpaVendorAdapter;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application extends JpaBaseConfiguration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+	@Override
+	protected AbstractJpaVendorAdapter createJpaVendorAdapter() {
+		EclipseLinkJpaVendorAdapter adapter = new EclipseLinkJpaVendorAdapter();
+		return adapter;
+	}
+
+	@Override
+	protected Map<String, Object> getVendorProperties() {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		return map;
+	}
+}
---
+++ SPR-12528/src/main/java/demo/PersonRepository.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2002-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package demo;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+	List<Person> findByLastName(@Param("name") String name);
+
+}
