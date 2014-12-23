b8f3df930c4cf0391f3c6804609da5d5e88a150a
Sebastien Deleuze <sdeleuze@pivotal.io>
2014-12-23 11:55:36 +0100

    Use WebAppInitializer instead of web.xml in SPR-12558

---
+++ SPR-12558/src/main/java/org/springframework/issues/config/WebAppInitializer.java
@@ -0,0 +1,21 @@
+package org.springframework.issues.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { WebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+}
