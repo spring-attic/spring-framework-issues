5115932e840f5d2d2495150d8ccfee1f919ea5d7
Sebastien Deleuze <sdeleuze@pivotal.io>
2015-06-01 15:52:26 +0200

    Add repro project for SPR-13078

---
+++ SPR-13078/src/main/java/org/springframework/issues/config/WebConfig.java
@@ -0,0 +1,19 @@
+package org.springframework.issues.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+@ComponentScan(basePackages="org.springframework.issues")
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+}
