39b5a43042ef84711061bbf3dd5ecd7242b315ad
Sebastien Deleuze <sdeleuze@pivotal.io>
2015-08-25 00:25:32 +0200

    Update SPR-13379 to use Spring Security

--- SPR-13379/src/main/java/org/springframework/issues/ScriptTemplateController.java
+++ SPR-13379/src/main/java/org/springframework/issues/ScriptTemplateController.java
@@ -16,18 +16,16 @@
 
 package org.springframework.issues;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-import javax.servlet.http.HttpServletResponse;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 @Controller
 public class ScriptTemplateController {
@@ -35,8 +33,8 @@ public class ScriptTemplateController {
     private static final Logger logger = LoggerFactory
             .getLogger(ScriptTemplateController.class);
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String home(Locale locale, Model model,HttpServletResponse response) {
+    @RequestMapping(path = {"", "security-with*"}, method = RequestMethod.GET)
+    public String home(Locale locale, Model model) {
         logger.info("Welcome home! The client locale is {}.", locale);
 
         DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,
