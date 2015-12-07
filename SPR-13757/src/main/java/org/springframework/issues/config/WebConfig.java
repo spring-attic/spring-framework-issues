edfc35adf649634ea95548581c4fe3e7613f6c85
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2015-12-06 20:20:01 -0500

    Add repro project for SPR-13757

---
+++ SPR-13757/src/main/java/org/springframework/issues/config/WebConfig.java
@@ -0,0 +1,39 @@
+package org.springframework.issues.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.VersionResourceResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@ComponentScan(basePackages="org.springframework.issues")
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("").setViewName("home");
+		registry.addViewController("/").setViewName("home");
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/")
+				.resourceChain(false)
+				.addResolver(new VersionResourceResolver().addContentVersionStrategy("/**"));
+	}
+
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+	
+}
---
+++ SPR-13757/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,12 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <title>Home</title>
+  <link href="<c:url value="/resources/main.css" />" rel="stylesheet"  type="text/css" />
+</head>
+<body>
+  <p>Home</p>
+</body>
+</html>
