8ce958bb74f79c4156d0c8f7b5941caac86d5a86
Brian Clozel <bclozel@pivotal.io>
2016-06-16 11:27:24 +0200

    Add repro project for SPR-14368

---
+++ SPR-14368/src/main/java/org/springframework/issues/config/WebConfig.java
@@ -0,0 +1,19 @@
+package org.springframework.issues.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+@ComponentScan(basePackages="org.springframework.issues")
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**")
+				.addResourceLocations("classpath:/static/");
+	}
+}
