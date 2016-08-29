7d3a0632e95c10c829c91a2e962fb46c3cbd08be
Brian Clozel <bclozel@pivotal.io>
2016-08-29 16:02:50 +0200

    Add repro project for SPR-14619

---
+++ SPR-14619/src/main/java/org/springframework/issues/config/WebConfig.java
@@ -0,0 +1,45 @@
+package org.springframework.issues.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@ComponentScan(basePackages="org.springframework.issues")
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**")
+				.addResourceLocations("classpath:/static/");
+	}
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		configurer.favorPathExtension(true)
+				.ignoreAcceptHeader(false)
+				.useJaf(false)
+				.defaultContentType(MediaType.TEXT_HTML)
+				.mediaType("html", MediaType.TEXT_HTML)
+				.mediaType("xml", MediaType.APPLICATION_XML)
+				.mediaType("json", MediaType.APPLICATION_JSON);
+		/*
+				.mediaType("woff2", new MediaType("application", "font-woff"))
+				.mediaType("woff", new MediaType("application", "font-woff"));
+				*/
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+}
