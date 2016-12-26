184f0876a7aaa8f2d71c82db00dddb10a018f296
Marvin Schramm <marvin@kreait.com>
2016-12-26 21:16:24 +0100

    Add repro project for SPR-15055

---
+++ SPR-15055/src/main/java/org/springframework/issues/SPR15055/Application.java
@@ -0,0 +1,15 @@
+package org.springframework.issues.SPR15055;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Describe this classes function
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
