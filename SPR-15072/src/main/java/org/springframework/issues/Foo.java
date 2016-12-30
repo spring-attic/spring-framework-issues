87eae0713e187d76f1b29640820d154d5fc73254
c-senthilkumar <csenthil.tech@gmail.com>
2016-12-30 11:05:30 +0530

    Add repro project for SPR-15072

---
+++ SPR-15072/src/main/java/org/springframework/issues/Foo.java
@@ -0,0 +1,13 @@
+package org.springframework.issues;
+
+public class Foo {
+    private Bar bar;
+
+    public Bar getBar() {
+        return this.bar;
+    }
+
+    public void setBar(Bar bar) {
+        this.bar = bar;
+    }
+}
