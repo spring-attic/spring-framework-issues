8c42bcc608fbfa76e5246cc277d3523167d9d9bb
myhr <mykola.hryhoriev@playtech.com>
2017-01-18 17:36:55 +0200

    Add repro project for SPR-15161

---
+++ SPR-15161/src/main/java/org/springframework/issues/Person.java
@@ -0,0 +1,51 @@
+package org.springframework.issues;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Person {
+
+    private final String name;
+
+    private final int age;
+
+    public Person(@JsonProperty("name") String name, @JsonProperty("age") int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (age != person.age) return false;
+        return name != null ? name.equals(person.name) : person.name == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + age;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
