58373764c89326359bf0a9a8584e1d306973f301
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2017-07-19 15:18:19 +0200

    Update SPR-15790 to send String's

--- SPR-15790/src/test/java/com/example/spr15790/TestClientApp.java
+++ SPR-15790/src/test/java/com/example/spr15790/TestClientApp.java
@@ -13,11 +13,22 @@ public class TestClientApp {
 	public static void main(String[] args) {
 
 		int toBeSent = 2;
-		Flux<String> flux = Flux.range(1, toBeSent).map(count -> "{\"content\":" + "\"" + count + "\"}");
+		Flux<String> flux = Flux.range(1, toBeSent).map(count -> {
+			String result = "{\"content\":" + "\"" + count + "\"}";
+			if (count == 1) {
+				return "[" + result;
+			}
+			else if (count == toBeSent) {
+				return "," + result + "]";
+			}
+			else {
+				return "," + result;
+			}
+		});
 
 		EntityExchangeResult<Void> result = WebTestClient.bindToServer().build()
 				.post().uri(URI.create("http://localhost:8080/eventSink"))
-				.contentType(MediaType.parseMediaType("application/stream+json;charset=UTF-8"))
+				.contentType(MediaType.APPLICATION_JSON)
 				.body(flux, String.class).exchange()
 				.expectStatus().isOk()
 				.expectBody().isEmpty();
