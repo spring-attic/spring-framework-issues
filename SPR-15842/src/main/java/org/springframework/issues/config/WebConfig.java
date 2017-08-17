4b65c7af098bbc78d266399f4aea7c08ef5d30bf
Martin Burchard <spring@martin-burchard.de>
2017-08-17 22:44:18 +0200

    add MappingJackson2HttpMessageConverter manually

--- SPR-15842/src/main/java/org/springframework/issues/config/WebConfig.java
+++ SPR-15842/src/main/java/org/springframework/issues/config/WebConfig.java
@@ -1,15 +1,19 @@
 package org.springframework.issues.config;
 
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @EnableWebMvc
-@ComponentScan(basePackages="org.springframework.issues")
+@ComponentScan(basePackages = "org.springframework.issues")
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 
@@ -17,7 +21,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("home");
 	}
-	
+
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -25,5 +29,19 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
 	}
-	
+
+	@Bean
+	public MappingJackson2HttpMessageConverter jsonMessageConverter() {
+		final MappingJackson2HttpMessageConverter mc = new MappingJackson2HttpMessageConverter();
+		final ObjectMapper om = new ObjectMapper();
+		mc.setObjectMapper(om);
+		return mc;
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		System.out.println(String.format("Available Message Converters: %s", converters.size()));
+		converters.add(jsonMessageConverter());
+	}
+
 }
